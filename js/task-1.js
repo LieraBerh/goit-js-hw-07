0ad43df56f837a1ae8aa5ba83e729d8dc68c16e5
LieraBerh <lierrastern@gmail.com>
2024-05-08 14:11:30 +0200

    upd

--- js/task-1.js
+++ js/task-1.js
@@ -2,36 +2,16 @@
 
 const ulElem = document.querySelector('#categories');
 
-console.log(`Number of categories: ${ulElem.children.length}`);
+const ulItems = ulElem.querySelectorAll('li.item');
+
+console.log(`Number of categories: ${ulItems.length}`);
 
 // ============================================
 
-const headerElem1 = ulElem.children[0].querySelector('h2');
+ulItems.forEach(item => {
+  const categoryTitle = item.querySelector('h2').textContent;
+  const numberCategoryItems = item.querySelectorAll('li').length;
 
-console.log(`Category: ${headerElem1.textContent}`);
-
-const category1ListElem = ulElem.children[0].querySelector('ul');
-
-console.log(`Elements: ${category1ListElem.children.length}`);
-
-// ============================================
-
-const headerElem2 = ulElem.children[1].querySelector('h2');
-
-console.log(`Category: ${headerElem2.textContent}`);
-
-const category2ListElem = ulElem.children[1].querySelector('ul');
-
-console.log(`Elements: ${category2ListElem.children.length}`);
-
-// ============================================
-
-const headerElem3 = ulElem.children[2].querySelector('h2');
-
-console.log(`Category: ${headerElem3.textContent}`);
-
-const category3ListElem = ulElem.children[2].querySelector('ul');
-
-console.log(`Elements: ${category3ListElem.children.length}`);
-
-// ============================================
+  console.log(`Category: ${categoryTitle}`);
+  console.log(`Elements: ${numberCategoryItems}`);
+});
